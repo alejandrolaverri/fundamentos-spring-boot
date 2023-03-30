4eff6050dd92afe4ce8087e75b742dc41fc704a7
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2023-03-30 11:21:04 +0200

    Uso de JPQL en anotación Query

--- src/main/java/com/alejandromo/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/alejandromo/springboot/fundamentos/FundamentosApplication.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -51,6 +52,16 @@ public class FundamentosApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// ejemplosAnteriores();
 		saveUsersInDatabase();
+		getInformationJpqlFromUser();
+	}
+
+	private void getInformationJpqlFromUser() {
+		LOGGER.info("Usuario con el metodo findByUserEmail: " +
+				userRepository.findByUserEmail("julie@gmail.com")
+				.orElseThrow(() -> new RuntimeException("No se encontro el usuario")));
+
+		userRepository.findAndSort("Luis", Sort.by("id").descending())
+				.forEach(user -> LOGGER.info("Usuario con metodo sort: " + user));
 	}
 
 	private void saveUsersInDatabase() {
